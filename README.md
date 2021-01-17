6940f7a1bbb8019f02323e223e4a64238823fbfa
Plasmatic <moses@mosesxu.net>
2021-01-17 16:19:42 -0500

    done???

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dmoj-badge
- DM::OJ rating SVG badges
+DM::OJ rating SVG badges
+
+Inspiration: KAIST CF/TC/Atcoder badges.
+
+# Usage
+
+(URL once it gets uploaded lol)
+
+Markdown: `![DM::OJ Badge](http://mosesxu.ca/badges/dmoj/<your handle>.svg)`
--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+import os
+from dmoj_utils import RequestHandler, rating_color
+from flask import Flask, redirect
+from urllib.parse import quote_plus
+from settings import DMOJ_ICON_ESC, DMOJ_ESC
 
 app = Flask(__name__)
+request_handler = RequestHandler()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<path>')
+def hello_world(path):
+    handle, ext = os.path.splitext(path)
+    if ext != '.svg':
+        return 'Requested file must be svg format', 404
+
+    try:
+        rating = request_handler.get_rating(handle)
+        return redirect(f'https://badgen.net/badge/{DMOJ_ESC}/{quote_plus(str(rating))}/{rating_color(rating)}?'
+                        f'icon={DMOJ_ICON_ESC}')
+    except KeyError:
+        return f'Could not get rating of handle {handle}', 500
 
 
 if __name__ == '__main__':
